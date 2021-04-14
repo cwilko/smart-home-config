0b6615b73a978d5d4e070f7ffd048d8ffb510f72
cwilko <chris.wilkinson@uk.ibm.com>
2021-04-14 22:18:57 +0100

    ARM64 and flux updates

--- workloads/kapacitor/deploy.sh
+++ workloads/kapacitor/deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-scp -r scripts/* root@192.168.1.2:/home/kubernetes/alerting-kapacitor-scripts-pvc*/
+scp -r ../../scripts/* root@192.168.1.2:/home/kubernetes/alerting-kapacitor-scripts-pvc*/
 
 kubectl delete pod -l app=kapacitor --namespace=alerting
