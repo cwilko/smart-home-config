4f81f451a2246862b5ae029ea7e706f895561fcd
cwilko <chris.wilkinson@uk.ibm.com>
2021-06-06 14:07:33 +0100

    Move to nodered projects

--- workloads/smart-home-actions/config/upload.sh
+++ workloads/smart-home-actions/config/upload.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-scp -r flows.json root@192.168.1.2:/home/kubernetes/actions-actions-data-pvc*/
+scp -r settings.js root@192.168.1.2:/home/kubernetes/actions-actions-data/
 
 kubectl delete pod -l app=actions --namespace=actions
