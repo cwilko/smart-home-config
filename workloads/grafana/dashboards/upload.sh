f7f27df0ec7ec8b3d84ba341490dabaa0dcae4f3
cwilko <chris.wilkinson@uk.ibm.com>
2021-06-10 23:49:23 +0100

    Updated grafana storage location

--- workloads/grafana/dashboards/upload.sh
+++ workloads/grafana/dashboards/upload.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-scp *.json root@192.168.1.2:/home/kubernetes/grafana-grafana-data-pvc*/
+scp *.json root@192.168.1.2:/home/kubernetes/grafana-grafana-data/
 
 kubectl delete pod -l app=grafana --namespace=grafana
