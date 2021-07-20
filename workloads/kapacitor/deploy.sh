92c61fe42aa915e3d8f10c367ad1435c62ac4b1d
cwilko <chris.wilkinson@uk.ibm.com>
2021-07-20 11:17:36 +0100

    Update heating info

--- workloads/kapacitor/deploy.sh
+++ workloads/kapacitor/deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-scp -r ../../scripts/* root@192.168.1.2:/home/kubernetes/alerting-kapacitor-scripts-pvc*/
+scp -r ../../scripts/* root@192.168.1.2:/home/kubernetes/alerting-kapacitor-scripts/
 
 kubectl delete pod -l app=kapacitor --namespace=alerting
