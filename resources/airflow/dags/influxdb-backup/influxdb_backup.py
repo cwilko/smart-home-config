9531c64ecee9b5845b2fc8db9d7c03d40bb870cb
cwilko <chris.wilkinson@uk.ibm.com>
2023-07-16 21:17:37 +0100

    backup job

---
+++ resources/airflow/dags/influxdb-backup/influxdb_backup.py
@@ -0,0 +1,27 @@
+import pendulum
+
+# from kubernetes.client import models as k8s
+
+from airflow import DAG
+from airflow.kubernetes.secret import Secret
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+
+dag = DAG(
+    "influxdb_backup",
+    schedule_interval="0 1 * * *",
+    start_date=pendulum.datetime(2023, 1, 1, tz="Europe/London"),
+    catchup=False,
+    is_paused_upon_creation=False,
+    tags=["storage"],
+)
+
+influxdb_backup = KubernetesPodOperator(
+    dag=dag,
+    pod_template_file="/opt/airflow/sync/smart-home-config/resources/airflow/pod_templates/influxdb_pod_template.yaml",
+    task_id="influxdb_backup",
+    namespace="storage",
+    name="influxdb-backup",
+    is_delete_operator_pod=True,
+    hostnetwork=False,
+    get_logs=True,
+)
