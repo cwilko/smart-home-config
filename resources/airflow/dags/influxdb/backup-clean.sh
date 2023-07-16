e98c33e978238a0aa2b4e510611e468e4a3ad75e
cwilko <chris.wilkinson@uk.ibm.com>
2023-07-16 17:02:30 +0100

    Custom airflow image

--- resources/airflow/dags/influxdb/backup-clean.sh
+++ resources/airflow/dags/influxdb/backup-clean.sh
@@ -3,3 +3,4 @@
 # kubectl create configmap backup-clean-config -n storage --dry-run=client --from-file=backup-clean.sh=backup-clean.sh  -o yaml > backup-clean-config.yaml
 
 sshpass -p ${NAS_PASSWORD} ssh -o StrictHostKeyChecking=no ${NAS_USER}@${NAS_HOST} '''find /home/kubernetes/storage-influxdb-backup*/ -type f -mtime +7 -execdir rm -v -- '{}' \;'''
+echo "Cleanup complete"
