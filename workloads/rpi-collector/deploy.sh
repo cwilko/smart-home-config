99939e9da5dfb97c1dba7687ab4e88cfc5f90657
cwilko <chris.wilkinson@uk.ibm.com>
2025-07-31 21:24:03 +0100

    Tidy up metrics

--- workloads/rpi-collector/deploy.sh
+++
@@ -1,6 +0,0 @@
-kubectl delete -f telegraf-config.yaml
-kubectl delete -f telegraf-deployment.yaml
-kubectl delete -f telegraf-rbac.yaml
-kubectl apply -f telegraf-rbac.yaml
-kubectl apply -f telegraf-config.yaml
-kubectl apply -f telegraf-deployment.yaml
