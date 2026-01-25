4781c10e2247169e3beaa8318044ebf2e72608eb
cwilko <chris.wilkinson@uk.ibm.com>
2026-01-25 22:53:50 +0000

    Migrate influxdb storage to nvme-of

--- workloads/storage/influxdb/upload.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-scp -r data/* root@192.168.1.2:/home/kubernetes/storage-influxdb-data-pvc*/
-
-#kubectl exec -it influxdb-deployment-6dd74886b7-htrcj -n storage -- sh /var/lib/influxdb/clear_subs.sh
